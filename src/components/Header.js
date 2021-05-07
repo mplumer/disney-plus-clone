c2a0d321aba07e3f4da04d41c4361c26caf30fb6
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-05-07 13:20:01 -0400

    labels/logos on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,73 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import styled from 'styled-components'
 
 function header() {
     return (
-        <Container>
-            Header
-        </Container>
+        <Nav>
+            <Logo src="/images/logo.svg" />
+
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a><a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
+
+            </NavMenu>
+        </Nav>
     )
 }
 
 export default header
 
 
-const Container = styled.div`
+const Nav = styled.div`
     height:70px;
     background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`
+
+const Logo = styled.img`
+    width: 80px;
+`
+
+const NavMenu = styled.div`
+    display: flex;
+
+    a {
+        display:flex;
+        align-items: center;
+        padding: 0 12px;
+        
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
 `

9f55e5c072769de167f8581660950ee9d1d45de7
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-05-06 22:08:45 -0400

    initial framework

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function header() {
+    return (
+        <Container>
+            Header
+        </Container>
+    )
+}
+
+export default header
+
+
+const Container = styled.div`
+    height:70px;
+    background: #090b13;
+`

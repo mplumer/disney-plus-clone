64c46e5b482514d4d6d0109c316a296765be4027
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-05-20 23:11:08 -0400

    login styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,95 +1,105 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import styled from 'styled-components'
+import styled from "styled-components";
 
-function Login() {
-    return (
-        <Container>
-            <CTA>
-                <CTALogoOne src="/images/cta-logo-one.svg" />
-                <SignUp>GET ALL THERE</SignUp>
-                <Description>
-                    This clone was made by Maxwell Plumer. If you're looking for a full-stack developer, capable of producing "Disney-Quality" content -- reach out via <a href="https://www.linkedin.com/in/maxplumer/">LinkedIn</a>!
-                </Description>
-                <CTALogoTwo src="/images/cta-logo-two.png" />
-            </CTA>
-        </Container>
-    )
-}
+const Login = (props) => {
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
 
-export default Login
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
 
-const Container = styled.div`
-    height: calc(100vh - 70px);
-    position: relative;
-    display: flex;
-    align-items: top;
-    justify-content: center;
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
 
-    &:before {
-        background-position: top;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-image: url("/images/login-background.jpg");
-        position: absolute;
-        content: "";
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        opacity: 0.7;
-        z-index: -1;
-    }
-
-
-`
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
 
 const CTA = styled.div`
-    max-width: 650px;
-    padding: 80px 40px;
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-    margin-top: 100px;
-    align-items: center;
-`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
 const CTALogoOne = styled.img`
-    
-`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
 
 const SignUp = styled.a`
-    width: 100%;
-    background-color: #0063e5;
-    font-weight: bold;
-    padding: 17px 0px;
-    color: #f9f9f9;
-    border-radius: 4px;
-    text-align: center;
-    font-size: 18px;
-    cursor: pointer;
-    transition: all 250ms;
-    letter-spacing: 1.5px;
-    margin-top: 8px;
-    margin-Bottom: 12px;
-
-
-    &:hover {
-        background: #0483ee;
-    }
-`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
 
 const Description = styled.p`
-    font-size: 16px;
-    letter-spacing: 1.5px;
-    text-align: center;
-    line-height: 1.5;
-
-
-
-`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
 
 const CTALogoTwo = styled.img`
-    width: 90%;
-    
-`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+export default Login;
